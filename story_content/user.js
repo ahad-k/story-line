33ca3a918993ca8b177725220c4c19a3a724e1c2
ahad-k <ahadkahvand3@gmail.com>
2025-12-16 23:55:21 +0330

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,48 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6GGDoyDVaib":
-        Script1();
-        break;
   }
-}
-
-function Script1()
-{
-  var player = GetPlayer();
-     var timeLeft = player.GetVar("Timer") || 60;
-     player.SetVar("Timer", timeLeft);
-     player.SetVar("GameOver", false);
-
-     if (window.timerInterval) {
-         clearInterval(window.timerInterval);
-     }
-
-     window.timerInterval = setInterval(function() {
-         var isPaused = player.GetVar("PauseTimeline");
-         var soundFinished = player.GetVar("SoundFinished") || false;
-         var playSound = player.GetVar("PlaySound") || false; // متغیر جدید
-         console.log("PauseTimeline: " + (isPaused !== null ? isPaused : "undefined") + ", TimeLeft: " + timeLeft + ", SoundFinished: " + soundFinished + ", PlaySound: " + playSound);
-         if (isPaused === true || soundFinished === true) {
-             clearInterval(window.timerInterval);
-             return;
-         } else if (timeLeft > 0) {
-             timeLeft--;
-             player.SetVar("Timer", timeLeft);
-             // شرط 10 ثانیه آخر و PlaySound
-             if (timeLeft <= 10 && playSound === true) {
-                 var clickSound = document.getElementById("clickSound"); // اسم فایل صوتی
-                 if (clickSound && clickSound.paused) { // فقط اگه متوقف باشه پخش کن
-                     clickSound.currentTime = 0; // از اول پخش کن
-                     clickSound.play().catch(function(error) {
-                         console.log("Error playing sound: " + error);
-                     });
-                 }
-             }
-         } else {
-             player.SetVar("GameOver", true);
-             clearInterval(window.timerInterval);
-         }
-     }, 1000);
 }
 
